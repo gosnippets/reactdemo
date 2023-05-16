3e0042304627acd6027f4765057587c7577aa3e3
Ramdas Gupta <er.rdgupta@gmail.com>
2023-05-16 09:38:16 +0530

    Fetch Post method

--- src/API_Fetch_Axios/ApiCollection.jsx
+++ src/API_Fetch_Axios/ApiCollection.jsx
@@ -1,16 +1,42 @@
+import { API_BASE_URL } from "../constants/Constatnts";
 
 const userApiUrl = "https://jsonplaceholder.typicode.com/users/";
 
-export const getUsers = async() => {
-    const data = await fetch(userApiUrl).then((res) => res.json()).then((users) => {        
+export const getUsers = async () => {
+    const data = await fetch(userApiUrl).then((res) => res.json()).then((users) => {
         return users;
     })
     return data;
 }
 
-export const getUser = async(id) => {
-    const data = await fetch(userApiUrl+id).then((res) => res.json()).then((user) => {        
+export const getUser = async (id) => {
+    const data = await fetch(userApiUrl + id).then((res) => res.json()).then((user) => {
         return user;
     })
     return data;
+}
+
+// ================= Account API ========================
+
+export const getAccounts = async () => {
+    const data = await fetch(API_BASE_URL + "list").then((res) => res.json()).then((accounts) => {
+        return accounts;
+    })
+    return data;
+}
+
+export const getAccount = async (id) => {
+}
+
+export const createAccount = async (data) => {
+    console.log("Data", data);
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }
+    const resData = await fetch(API_BASE_URL + "addaccount", requestOptions).then((res) => res.json()).then((account) => {
+        return account;
+    })
+    return resData;
 }
--- src/API_Fetch_Axios/FetchExample.jsx
+++ src/API_Fetch_Axios/FetchExample.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { getUser, getUsers } from './ApiCollection';
+import { Container, Table, TableHead, TableCell, TableContainer, TableBody, TableRow, Paper, Tooltip } from '@mui/material'
+import React, { useEffect, useRef, useState } from 'react'
+import { createAccount, getAccounts, getUser, getUsers } from './ApiCollection';
 import { Card, CardContent, Typography } from '@mui/material';
 
-const userApiUrl = "https://jsonplaceholder.typicode.com/users";
 
 export default function FetchExample() {
     const [userList, setUserList] = useState([])
+    const [accountList, setAccountList] = useState([])
+    const [accountKeys, setAccountKeys] = useState([])
     const [user, setUser] = useState()
+
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
     useEffect(() => {
         getAllUsers();
+        getAllAccounts();
     }, []);
+
+    const getAllAccounts = async () => {
+        const data = await getAccounts();
+        setAccountList(data)
+        setAccountKeys(Object.keys(data))
+    }
 
     const getAllUsers = async () => {
         const data = await getUsers();
@@ -20,6 +33,18 @@ export default function FetchExample() {
     const getSingleUser = async (id) => {
         const data = await getUser(id);
         setUser(data);
+    }
+
+    const handleSubmit = async() => {
+        const username = usernameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(username, email, password)
+        if (username && email && password) {
+            const resData = await createAccount({ username, email, password })
+            console.log("ResData", resData);
+            getAllAccounts()
+        }
     }
 
 
@@ -41,6 +66,40 @@ export default function FetchExample() {
                     </CardContent>
                 </Card>
             )}
+
+            <input type='text' name="username" ref={usernameRef} placeholder='Username' />
+            <input type='email' name="email" ref={emailRef} placeholder='Email' />
+            <input type='password' name="password" ref={passwordRef} placeholder='Password' />
+            <button onClick={handleSubmit}>Add Account</button>
+
+
+            <h2> Account List</h2>
+            <TableContainer component={Paper} className='my-5'>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">ID</TableCell>
+                            <TableCell>Username</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Password</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {accountKeys.map((id) => (
+                            <TableRow
+                                key={id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row" align="right">{id} </TableCell>
+                                <TableCell >{accountList[id].username}</TableCell>
+                                <TableCell >{accountList[id].email}</TableCell>
+                                <TableCell>{accountList[id].password}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
         </div>
     )
 }
